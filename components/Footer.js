3a3f1b72ea1e580a1a10a9a0f11780915083a698
gugulothbhanuprakash2206 <gugulothbhanuprakash606@gmail.com>
2025-11-19 16:02:09 +0530

    Add files via upload

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="w-full border-t border-white/10 bg-black mt-auto">
+      <div className="max-w-7xl mx-auto px-4 py-8 text-center text-white/70 text-sm">
+        © 2025 Melody Master. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
